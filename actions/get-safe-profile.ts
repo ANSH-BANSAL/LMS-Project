a658d125be4f67e6161c972a58417b22e35c17d9
Ansh Bansal <anshofficial5255@gmail.com>
2024-08-25 19:44:43 +0530

    course dashboard

---
+++ actions/get-safe-profile.ts
@@ -0,0 +1,46 @@
+import { db } from "@/lib/db";
+import { SafeProfile } from "@/type";
+import { auth, currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+export default async function getSafeProfile() {
+  try {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const currentProfile = await db.profile.findUnique({
+        where: {
+          userId,
+        },
+        select: {
+          id: true,
+          userId: true,
+          name: true,
+          imageUrl: true,
+          email: true,
+          role: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+      
+      if (!currentProfile) {
+        return null;
+      }
+      
+      const safeProfile: SafeProfile = {
+        ...currentProfile,
+        createdAt: currentProfile.createdAt.toISOString(),
+        updatedAt: currentProfile.updatedAt.toISOString(),
+      };
+
+       
+    return safeProfile;
+  } catch (error: any) {
+    return null;
+  }
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,22 +1,31 @@
+import { SidebarIcon } from "lucide-react";
+// import { SideBar } from "./_components/SideBar";
+import SideBar from "./_components/SideBar"
 import { Navbar } from "./_components/Navbar";
-import SideBar from "./_components/SideBar";
+import { redirect } from "next/navigation";
+import getSafeProfile  from "@/actions/get-safe-profile";
 
-const DashBoardLayout = ({
-    children
-} : ({children : React.ReactNode})) => {
-    return ( 
-        <div className="h-full">
-            <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-                <Navbar />
-            </div>
-            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-                <SideBar/>
-            </div>
-            <main className="md:pl-56 pt-[80px] h-full ">
-                {children}
-            </main>
-        </div>
-     );
-}
- 
-export default DashBoardLayout;
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const safeProfile = await getSafeProfile();
+
+  if (!safeProfile) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full dark:bg-gray-900">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50 dark:bg-gray-900">
+        <Navbar currentProfile={safeProfile} />
+      </div>
+
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50 dark:bg-gray-900">
+        <SideBar />
+      </div>
+      <main className="md:pl-56 pt-[80px] h-full dark:bg-gray-900">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
